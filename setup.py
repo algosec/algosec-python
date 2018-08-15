3c281350266ac9e0f64e5c1a4dad5688b1d8ca1d
almogcohen <almog1232@gmail.com>
2018-08-15 16:31:11 -0700

    add python 3 support to setup.py

--- setup.py
+++ setup.py
@@ -24,7 +24,12 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Natural Language :: English",
         "Topic :: Software Development :: Libraries :: Python Modules",
+        "Programming Language :: Python :: 2",
         "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
     ],
     install_requires=[
         "requests",
@@ -33,4 +38,5 @@ setup(
         "suds_requests",
         "ipaddress",
     ],
+    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*',
 )
