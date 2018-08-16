62594dee5884a0b607cf42b079ffd127e170ea61
almogcohen <almog1232@gmail.com>
2018-08-16 10:04:07 -0700

    add python 3 support to travis.yml and setup.py

--- setup.py
+++ setup.py
@@ -26,10 +26,10 @@ setup(
         "Topic :: Software Development :: Libraries :: Python Modules",
         "Programming Language :: Python :: 2",
         "Programming Language :: Python :: 2.7",
-        # "Programming Language :: Python :: 3",
-        # "Programming Language :: Python :: 3.4",
-        # "Programming Language :: Python :: 3.5",
-        # "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
     ],
     install_requires=[
         "requests",
@@ -38,6 +38,5 @@ setup(
         "suds_requests",
         "ipaddress",
     ],
-    # python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*',
-    python_requires='~=2.7',
+    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*',
 )
