be02d8fc2dc79e828a348a4b096dd571ac254bae
almogcohen <almog1232@gmail.com>
2018-08-27 13:16:47 +0300

    Bump version: 1.0.1 → 1.0.2

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.rst") as f:
 
 setup(
     name="algosec",
-    version="1.0.1",
+    version="1.0.2",
     packages=["algosec"],
     url="https://github.com/algosec/algosec-python",
     license="MIT",
